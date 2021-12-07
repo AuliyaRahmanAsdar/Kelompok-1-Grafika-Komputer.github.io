2f5e9e7c6f146d2e8bdd6d626c6e066c2876bb84
Rafiud17 <39791692+Rafiud17@users.noreply.github.com>
2021-12-07 16:03:58 +0800

    Menambahkan Comment/keterangan pada masing-masing syntaks

--- Grafkom_Kelompok1.py
+++ Grafkom_Kelompok1.py
@@ -1,3 +1,5 @@
+# Numpy merupakan singkatan Numerical Python yang digunakan dalam memproses array
+# Matplotlib digunakan untuk menggambarkan titik koordinat menjadi sebuah garis
 import numpy as np
 import matplotlib.pyplot as grafik
 
@@ -7,9 +9,15 @@ y1 = int(input('Masukan Nilai Y1  : '))
 x2 = int(input('Masukan Nilai X2  : '))
 y2 = int(input('Masukan Nilai Y2  : '))
 print('------------------------------------------------')
-
+# koordinar awal x dan y
 x = x1
 y = y1
+# membuat pengkondisian dengan beberapa syarat seperti :
+
+# ---> Jika nilai x1 = x2 (garis vertikal), maka :
+#       a. y = y + 1 dan x tetap
+#       b. gambar titik kordinat (x,y) 
+#       c. tampilkan garis dari titik-titik koordinat yang terbentuk (selesai)
 
 if x1 == x2:
     Koor_x = []
@@ -17,6 +25,11 @@ if x1 == x2:
     for i in range (1,y2,1):
         grafik.plot(Koor_x,Koor_y)
         grafik.show()
+        
+# ---> Jika nilai y1 = y2 (garis horizontal), maka :
+#       a. x = x + 1 dan y tetap
+#       b. gambar titik kordinat (x,y)
+#       c. tampilkan garis dari titik-titik koordinat yang terbentuk (selesai)
 
 elif y1 == y2:
     koor_x = []
@@ -24,11 +37,19 @@ elif y1 == y2:
     for i in range (1,y2,1):
         grafik.plot(Koor_x,Koor_y)
         grafik.show()
+# ---> Jika ke-2 syarat di atas tidak memenuhi,  maka proses penentuan titik koordinat berlanjut ke:
+#       1. hitung kemiringan garis dengan m = (y2 - y1) / (x2 - x1) 
+#       2. iterasi diulang sebanyak N kali
+#       3. menentukan nilai i dengan persamaan y = m ( x - x1 ) + y1
+#       4. lakukan pembulatan pada  nilai y
+#       5. gambar titik (x, y(pembulatan))
+#       6. nilai x didapatkan dari persamaan x = x + 1
 
 else:
     koor_x = []
     koor_y = []
     m_kemiringangaris = (y2 - y1) / (x2 - x1)
+    # N adalah banyaknya iterasi yang didapatkan dari persamaan dibawah ini apabila  x1 != x2 atau y1 != y2
     N = x2 - x1 + 1
     for i in range (0,N,1):
         nilai_y = m_kemiringangaris * (x - x1) + y1
@@ -37,7 +58,9 @@ else:
         koor_x.append(x)
         koor_y.append(kordinatY)
         x+=1
-        
+# syntaks apppend digunakan untuk menambah item ke dalam array atau list
+# plt.plot digunakan agar mathplotlib dapat membuat titik pertemuan di masing-masing koordinat x,y
+# plt.show digunakan agar mathplotlib dapat menampilkan garis dari titik titik kordinat yang telah di dapat 
 
     grafik.plot(koor_x,koor_y)
     grafik.show()
