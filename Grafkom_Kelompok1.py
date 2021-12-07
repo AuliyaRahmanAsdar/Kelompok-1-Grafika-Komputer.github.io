6b5d1be58194a2fdd53903bbf56bd74fbd0d3214
fauzanalashar <77719142+fauzanalashar@users.noreply.github.com>
2021-12-07 15:38:14 +0800

    Mengubah Variabel

--- Grafkom_Kelompok1.py
+++ Grafkom_Kelompok1.py
@@ -2,42 +2,42 @@ import numpy as np
 import matplotlib.pyplot as grafik
 
 
-x1 = int(input('Masukan nilai x1  : '))
-y1 = int(input('Masukan nilai y1  : '))
-x2 = int(input('Masukan nilai x2  : '))
-y2 = int(input('Masukan nilai y2  : '))
+x1 = int(input('Masukan Nilai X1  : '))
+y1 = int(input('Masukan Nilai Y1  : '))
+x2 = int(input('Masukan Nilai X2  : '))
+y2 = int(input('Masukan Nilai Y2  : '))
 print('------------------------------------------------')
 
 x = x1
 y = y1
 
 if x1 == x2:
-    titikA = []
-    titikB = []
+    Koor_x = []
+    Koor_y = []
     for i in range (1,y2,1):
-        grafik.plot(titikA,titikB)
+        grafik.plot(Koor_x,Koor_y)
         grafik.show()
 
 elif y1 == y2:
-    titikA = []
-    titikB = []
+    koor_x = []
+    koor_y = []
     for i in range (1,y2,1):
-        grafik.plot(titikA,titikB)
+        grafik.plot(Koor_x,Koor_y)
         grafik.show()
 
 else:
-    titikA = []
-    titikB = []
+    koor_x = []
+    koor_y = []
     m_kemiringangaris = (y2 - y1) / (x2 - x1)
     N = x2 - x1 + 1
     for i in range (0,N,1):
         nilai_y = m_kemiringangaris * (x - x1) + y1
         kordinatY = round(nilai_y)
         print('Garis yang di lewati oleh titik A da titik B yaitu ', x,',', kordinatY)
-        titikA.append(x)
-        titikB.append(kordinatY)
+        koor_x.append(x)
+        koor_y.append(kordinatY)
         x+=1
         
 
-    grafik.plot(titikA,titikB)
+    grafik.plot(koor_x,koor_y)
     grafik.show()
