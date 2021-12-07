87e376caf8dbbd486e51ddf7ed22ea55add88130
rahmatlatif10 <77723387+rahmatlatif10@users.noreply.github.com>
2021-12-07 15:12:41 +0800

    Menambahkan Syarat Jika Kedua Syarat Sebelumnya Tidak Memenuhi

--- Grafkom_Kelompok1.py
+++ Grafkom_Kelompok1.py
@@ -24,3 +24,20 @@ elif y1 == y2:
     for i in range (1,y2,1):
         grafik.plot(titikA,titikB)
         grafik.show()
+
+else:
+    titikA = []
+    titikB = []
+    m_kemiringangaris = (y2 - y1) / (x2 - x1)
+    N = x2 - x1 + 1
+    for i in range (0,N,1):
+        nilai_y = m_kemiringangaris * (x - x1) + y1
+        kordinatY = round(nilai_y)
+        print('Garis yang di lewati oleh titik A da titik B yaitu ', x,',', kordinatY)
+        titikA.append(x)
+        titikB.append(kordinatY)
+        x+=1
+        
+
+    grafik.plot(titikA,titikB)
+    grafik.show()
