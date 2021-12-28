95a77fe8ab48512d1f2f794f2370183578c68937
nurhalisamadukubah <nurhalisamadukubah@gmail.com>
2021-12-28 11:44:28 +0800

    edit komentar line 2

--- Grafkom_Kelompok1.py
+++ Grafkom_Kelompok1.py
@@ -1,5 +1,5 @@
 # Numpy merupakan singkatan Numerical Python yang digunakan dalam memproses array... misi ngedit dikit
-# Matplotlib digunakan untuk menggambarkan titik koordinat menjadi sebuah garis
+# Matplotlib digunakan untuk menggambarkan titik koordinat menjadi sebuah garis... misi mau coba ngedit lagi
 import numpy as np
 import matplotlib.pyplot as graphic
 
